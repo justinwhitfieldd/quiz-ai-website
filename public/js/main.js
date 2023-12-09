b4193488c690a1b331f38bb4602a840165d71d48
justinwhitfieldd <justinwhitfieldd@gmail.com>
2023-12-09 12:21:34 -0600

    better nav

--- public/js/main.js
+++ public/js/main.js
@@ -36,7 +36,7 @@
             
             $('html, body').animate({
                 scrollTop: $(this.hash).offset().top - 45
-            }, 1500, 'easeInOutExpo');
+            }, 15, 'easeInOutExpo');
             
             if ($(this).parents('.navbar-nav').length) {
                 $('.navbar-nav .active').removeClass('active');
