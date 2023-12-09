0e20d4693c8692ff0d47fcc0e6ee27de9fc57f29
justinwhitfieldd <justinwhitfieldd@gmail.com>
2023-12-09 11:48:32 -0600

    work please

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDJB_VpURfiO2nPSSCqklpHqwh8Mu8myZI",
-  authDomain: "quiz-ai-website.firebaseapp.com",
-  projectId: "quiz-ai-website",
-  storageBucket: "quiz-ai-website.appspot.com",
-  messagingSenderId: "917006625639",
-  appId: "1:917006625639:web:9c10af75f1598c93079dc2",
-  measurementId: "G-LCJER8BX06"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 (function ($) {
     "use strict";
 
